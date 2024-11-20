9ad2fea074713fa3d8e139c49193b0b8fab64e6e
daniel <danielmedeiros.medeiros@gmail.com>
2024-11-20 11:47:51 -0300

    CaseMedica3

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('perfil/', views.perfil, name='perfil'),
     path('perfil/<str:username>/', views.visualizar_perfil, name='visualizar_perfil'),
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
